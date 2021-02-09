38ae5ddad19c4ab3eb2a39213915ffa7e5d6b1a4
Andrew <andrew.ku93@gmail.com>
2021-02-09 19:55:18 +0000

    complete the timer1 alarm beep function

---
+++ timer1.js
@@ -0,0 +1,21 @@
+const getArgs = () => {
+  const args = process.argv.slice(2);
+  return args;
+};
+
+const alarmBeep = (arr) => {
+  if (arr === undefined) {
+    return;
+  }
+  for (const item of arr) {
+    if (item < 0 || isNaN(item) === true) {
+    continue;
+    }
+    const interval = item * 1000;
+    setTimeout(() => {
+      process.stdout.write("\x07");
+    }, interval);
+  }
+};
+
+alarmBeep(getArgs());
